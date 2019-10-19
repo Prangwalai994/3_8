97d53dfb0aefcae93c766ae1aa035234a5105940
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-19 21:05:07 +0700

    Add project files.

---
+++ Testing_Private_Methods3_8/Fundamentals/ErrorLogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_Private_Methods3_8.Fundamentals
+{
+    public class ErrorLogger
+    {
+        public string LastError { get; set; }
+
+        public event EventHandler<Guid> ErrorLogged;
+
+        private Guid _errorId;
+
+        public void Log(string _error)
+        {
+            if (String.IsNullOrWhiteSpace(_error))
+                throw new ArgumentNullException();
+
+
+            LastError = _error;
+
+            // Write the log to a storage
+            // ...
+
+            OnErrorLogged(Guid.NewGuid());
+        }
+
+        public virtual void OnErrorLogged(Guid _errorId)
+        {
+            ErrorLogged?.Invoke(this, _errorId);
+        }
+    }
+}
